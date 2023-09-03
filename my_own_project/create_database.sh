a3085a0602c0556a7a4f57f8490916467fa20549
mohammed <mm994225@gmail.com>
2023-09-03 23:06:11 +0200

    create database

---
+++ my_own_project/create_database.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+shopt -s extglob
+while true
+do
+read -p "please enter name of database: " name
+if [ "$name" = "" ]
+then
+echo "you must enter name of database"
+elif [ -d $name ]
+then
+echo "database is founded chose other name"
+else
+case $name in
++([a-zA-Z0-9_])) mkdir $name
+echo "$name database is added"
+break
+;;
+*)
+echo "please write name of database in write way"
+esac
+fi
+done
