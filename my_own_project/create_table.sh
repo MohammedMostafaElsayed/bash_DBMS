8f03804de73a03fc4707f0bc424eee1a38d2a69c
mohammed <mm994225@gmail.com>
2023-09-05 19:05:57 +0300

    create table and all check

--- my_own_project/create_table.sh
+++ my_own_project/create_table.sh
@@ -1,30 +1,48 @@
 #!/usr/bin/bash
-
+shopt -s extglob
+flag=0
 while true
 do
-read -p "enter table name: " table
-if [ "$table" = "" ]
+read -p "please enter table name: " name_tb
+if [ -z "$name_tb" ]
 then
-echo "you must enter table name"
-elif [ -f $name ]
-then
-echo "database is founded chose other name"
+	echo "you must enter table name"
 else
-case $name in
-+([a-zA-Z_]*)) mkdir -p ./DATABASES/$name
-echo "$name database is added"
-break
+if [ -f $name_tb ]
+then
+	echo "table is found chose other name"
+else
+case $name_tb in
++([a-zA-Z_]*)) 
+    touch $name_tb
+    echo "$name_tb table is created"
+    flag=1
+    break
 ;;
 *)
-echo "please write name of database in write way"
+    echo "please write name of table in right way"		
 esac
 fi
+fi
 done
-read -p "enter number of column: " num
-i=0
-while((i < num))
-do
-while true
-do
-read -p "enter $((i+1)) column name: " name
 
+if [ $flag -eq 1 ]
+then
+    data_type=("int" "char" "string")
+    read -p "plese enter number of colom: " num
+    i=0
+    while ((i < $num))
+    do
+        read -p "please enter name of column no $(($i+1)): " col_name 
+        read -p "plese ender data type: " col_type
+        array_col_name[i]=$col_name
+        row="$row$col_name"
+        if [ $(($i+1)) -ne $num ]
+        then
+            row="$row:" 
+        fi
+        array_col_type[i]=$col_type
+        i=$(($i+1))
+    done
+    echo "$row" >> $name_tb
+fi
--- my_own_project/menu2.sh
+++ my_own_project/menu2.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 
 secend_menu=("Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "prev menu")
+while true
+do
+read -p "please enter the name of database: " name
+if [ -d ./DATABASES/$name ]
+then
+cd ./DATABASES/$name
 select choies2 in "${secend_menu[@]}"
 do
 case $REPLY in
-1) . ./create_table.sh
+1) . ../../create_table.sh
 ;;
 2) echo "hi $choies2"
 ;;
@@ -19,8 +25,13 @@ case $REPLY in
 7) echo "hi $choies2"
 ;;
 8) echo "chose from first menu"
-break
+break 2
 ;;
 *) echo "the $REPLY is not found"
 esac
+done
+break
+else
+    echo "database not fount enter right name"
+fi
 done
